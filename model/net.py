89914f98560a74345306536e01b53e11edcb7be5
Aayush Pratap Singh <141538111+Aayushongit@users.noreply.github.com>
2025-04-26 22:53:17 +0530

    Update net.py
    Refactor CNN model: Replace BatchNorm with LayerNorm, add ResNet-style residual connections, implement pruning capability, enhance dropout pattern, add Kaiming weight initialization, and expand metrics with F1 score and top-k accuracy. Remove all comments for cleaner code.

--- model/net.py
+++ model/net.py
@@ -1,135 +1,155 @@
-"""
-   Baseline CNN, losss function and metrics
-   Also customizes knowledge distillation (KD) loss function here
-"""
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Net(nn.Module):
-    """
-    This is the standard way to define your own network in PyTorch. You typically choose the components
-    (e.g. LSTMs, linear layers etc.) of your network in the __init__ function. You then apply these layers
-    on the input step-by-step in the forward function. You can use torch.nn.functional to apply functions
-
-    such as F.relu, F.sigmoid, F.softmax, F.max_pool2d. Be careful to ensure your dimensions are correct after each
-    step. You are encouraged to have a look at the network in pytorch/nlp/model/net.py to get a better sense of how
-    you can go about defining your own network.
-
-    The documentation for all the various components available o you is here: http://pytorch.org/docs/master/nn.html
-    """
-
-    def __init__(self, params):
-        """
-        We define an convolutional network that predicts the sign from an image. The components
-        required are:
-
-        Args:
-            params: (Params) contains num_channels
-        """
-        super(Net, self).__init__()
-        self.num_channels = params.num_channels
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, stride=stride, padding=1)
+        self.ln1 = nn.LayerNorm([out_channels, 32//stride, 32//stride])
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1)
+        self.ln2 = nn.LayerNorm([out_channels, 32//stride, 32//stride])
         
-        # each of the convolution layers below have the arguments (input_channels, output_channels, filter_size,
-        # stride, padding). We also include batch normalisation layers that help stabilise training.
-        # For more details on how to use these layers, check out the documentation.
-        self.conv1 = nn.Conv2d(3, self.num_channels, 3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(self.num_channels)
-        self.conv2 = nn.Conv2d(self.num_channels, self.num_channels*2, 3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(self.num_channels*2)
-        self.conv3 = nn.Conv2d(self.num_channels*2, self.num_channels*4, 3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(self.num_channels*4)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride),
+                nn.LayerNorm([out_channels, 32//stride, 32//stride])
+            )
 
-        # 2 fully connected layers to transform the output of the convolution layers to the final output
-        self.fc1 = nn.Linear(4*4*self.num_channels*4, self.num_channels*4)
-        self.fcbn1 = nn.BatchNorm1d(self.num_channels*4)
-        self.fc2 = nn.Linear(self.num_channels*4, 10)       
+    def forward(self, x):
+        out = F.relu(self.ln1(self.conv1(x)))
+        out = self.ln2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class EnhancedNet(nn.Module):
+    def __init__(self, params):
+        super(EnhancedNet, self).__init__()
+        self.num_channels = params.num_channels
         self.dropout_rate = params.dropout_rate
+        self.enable_pruning = getattr(params, 'enable_pruning', False)
+        
+        self.conv1 = nn.Conv2d(3, self.num_channels, 3, stride=1, padding=1)
+        self.ln1 = nn.LayerNorm([self.num_channels, 32, 32])
+        
+        self.res1 = ResidualBlock(self.num_channels, self.num_channels*2, 2)
+        self.res2 = ResidualBlock(self.num_channels*2, self.num_channels*4, 2)
+        self.res3 = ResidualBlock(self.num_channels*4, self.num_channels*8, 2)
+        
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(self.num_channels*8, 10)
+        
+        self._initialize_weights()
+        self.masks = None
+        
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.constant_(m.bias, 0)
+    
+    def apply_pruning(self, pruning_rate=0.3):
+        if not self.enable_pruning:
+            return
+            
+        self.masks = {}
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                tensor = param.data.cpu().numpy()
+                threshold = np.percentile(np.abs(tensor), pruning_rate * 100)
+                mask = torch.FloatTensor(np.abs(tensor) > threshold).to(param.device)
+                self.masks[name] = mask
+                param.data *= mask
+                
+    def forward(self, x):
+        if self.masks is not None and self.enable_pruning:
+            for name, param in self.named_parameters():
+                if 'weight' in name and name in self.masks:
+                    param.data *= self.masks[name]
+                    
+        out = F.relu(self.ln1(self.conv1(x)))
+        out = self.res1(out)
+        
+        if self.training:
+            out = F.dropout2d(out, p=self.dropout_rate/2)
+            
+        out = self.res2(out)
+        
+        if self.training:
+            out = F.dropout2d(out, p=self.dropout_rate)
+            
+        out = self.res3(out)
+        out = self.adaptive_pool(out)
+        out = out.view(out.size(0), -1)
+        
+        if self.training:
+            out = F.dropout(out, p=self.dropout_rate)
+            
+        out = self.fc(out)
+        return out
 
-    def forward(self, s):
-        """
-        This function defines how we use the components of our network to operate on an input batch.
+def weighted_loss_fn(outputs, labels, weights=None):
+    if weights is None:
+        return nn.CrossEntropyLoss()(outputs, labels)
+    else:
+        return nn.CrossEntropyLoss(weight=torch.tensor(weights).to(outputs.device))(outputs, labels)
 
-        Args:
-            s: (Variable) contains a batch of images, of dimension batch_size x 3 x 32 x 32 .
-
-        Returns:
-            out: (Variable) dimension batch_size x 6 with the log probabilities for the labels of each image.
-
-        Note: the dimensions after each step are provided
-        """
-        #                                                  -> batch_size x 3 x 32 x 32
-        # we apply the convolution layers, followed by batch normalisation, maxpool and relu x 3
-        s = self.bn1(self.conv1(s))                         # batch_size x num_channels x 32 x 32
-        s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels x 16 x 16
-        s = self.bn2(self.conv2(s))                         # batch_size x num_channels*2 x 16 x 16
-        s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels*2 x 8 x 8
-        s = self.bn3(self.conv3(s))                         # batch_size x num_channels*4 x 8 x 8
-        s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels*4 x 4 x 4
-
-        # flatten the output for each image
-        s = s.view(-1, 4*4*self.num_channels*4)             # batch_size x 4*4*num_channels*4
-
-        # apply 2 fully connected layers with dropout
-        s = F.dropout(F.relu(self.fcbn1(self.fc1(s))), 
-            p=self.dropout_rate, training=self.training)    # batch_size x self.num_channels*4
-        s = self.fc2(s)                                     # batch_size x 10
-
-        return s
-
-
-def loss_fn(outputs, labels):
-    """
-    Compute the cross entropy loss given outputs and labels.
-
-    Args:
-        outputs: (Variable) dimension batch_size x 6 - output of the model
-        labels: (Variable) dimension batch_size, where each element is a value in [0, 1, 2, 3, 4, 5]
-
-    Returns:
-        loss (Variable): cross entropy loss for all images in the batch
-
-    Note: you may use a standard loss function from http://pytorch.org/docs/master/nn.html#loss-functions. This example
-          demonstrates how you can easily define a custom loss function.
-    """
-    return nn.CrossEntropyLoss()(outputs, labels)
-
+def focal_loss_fn(outputs, labels, gamma=2.0, alpha=0.25):
+    ce_loss = F.cross_entropy(outputs, labels, reduction='none')
+    pt = torch.exp(-ce_loss)
+    focal_loss = alpha * (1-pt)**gamma * ce_loss
+    return focal_loss.mean()
 
 def loss_fn_kd(outputs, labels, teacher_outputs, params):
-    """
-    Compute the knowledge-distillation (KD) loss given outputs, labels.
-    "Hyperparameters": temperature and alpha
-
-    NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
-    and student expects the input tensor to be log probabilities! See Issue #2
-    """
     alpha = params.alpha
     T = params.temperature
-    KD_loss = nn.KLDivLoss()(F.log_softmax(outputs/T, dim=1),
+    KD_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(outputs/T, dim=1),
                              F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T) + \
               F.cross_entropy(outputs, labels) * (1. - alpha)
-
     return KD_loss
 
-
 def accuracy(outputs, labels):
-    """
-    Compute the accuracy, given the outputs and labels for all images.
-
-    Args:
-        outputs: (np.ndarray) output of the model
-        labels: (np.ndarray) [0, 1, ..., num_classes-1]
-
-    Returns: (float) accuracy in [0,1]
-    """
     outputs = np.argmax(outputs, axis=1)
     return np.sum(outputs==labels)/float(labels.size)
 
+def top_k_accuracy(outputs, labels, k=5):
+    batch_size = labels.size
+    top_k_preds = np.argsort(outputs, axis=1)[:, -k:]
+    correct = 0
+    for i in range(batch_size):
+        if labels[i] in top_k_preds[i]:
+            correct += 1
+    return correct / float(batch_size)
 
-# maintain all metrics required in this dictionary- these are used in the training and evaluation loops
 metrics = {
     'accuracy': accuracy,
-    # could add more metrics such as accuracy for each token type
+    'top_5_accuracy': lambda outputs, labels: top_k_accuracy(outputs, labels, k=5),
+    'f1_score': lambda outputs, labels: compute_f1_score(np.argmax(outputs, axis=1), labels)
 }
+
+def compute_f1_score(predictions, labels, average='macro'):
+    unique_labels = np.unique(labels)
+    f1_scores = []
+    
+    for label in unique_labels:
+        true_positives = np.sum((predictions == label) & (labels == label))
+        false_positives = np.sum((predictions == label) & (labels != label))
+        false_negatives = np.sum((predictions != label) & (labels == label))
+        
+        precision = true_positives / (true_positives + false_positives + 1e-10)
+        recall = true_positives / (true_positives + false_negatives + 1e-10)
+        
+        f1 = 2 * precision * recall / (precision + recall + 1e-10)
+        f1_scores.append(f1)
+    
+    if average == 'macro':
+        return np.mean(f1_scores)
+    else:
+        return f1_scores
